25afeec64ef88cb1df083c802af1dd6e677f4e3e
Pooja Vaidya <poojavaidya0802@gmail.com>
2021-10-08 15:57:36 -0700

    ques6

---
+++ js/06-odd-or-even.js
@@ -0,0 +1,8 @@
+
+for (var i = 0; i <= 15; i += 1) {
+    if (i % 2 == 0) {
+        window.document.write(i + " is even<br>");
+    } else {
+        window.document.write(i + " is odd<br>");
+    }
+}
